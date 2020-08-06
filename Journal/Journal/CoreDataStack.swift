40acf8cdfce8038c146aeb42022dfe6173382be9
Zach Thacker <zachthac@aol.com>
2020-08-05 23:30:43 -0400

    Part 1 complete File Creation and UI

---
+++ Journal/Journal/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  Journal
+//
+//  Created by Clean Mac on 8/5/20.
+//  Copyright © 2020 LambdaStudent. All rights reserved.
+//
+
+import Foundation
---
+++ Journal/Journal/Entry+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Clean Mac on 8/5/20.
+//  Copyright © 2020 LambdaStudent. All rights reserved.
+//
+
+import Foundation
