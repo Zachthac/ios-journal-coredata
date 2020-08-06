baac87a3e5a2f270c44c97e722bbabac387dd674
Zach Thacker <zachthac@aol.com>
2020-08-05 22:10:34 -0400

    initial commit

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Clean Mac on 8/5/20.
+//  Copyright © 2020 LambdaStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
